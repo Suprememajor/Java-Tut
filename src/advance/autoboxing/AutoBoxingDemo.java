d57934f1aa78c86523affde1fc3f2e914dd29480
Suprememajor <ndipnobertetta@gmail.com>
2022-06-09 06:20:19 +0100

    Add tutorial on Autoboxing

---
+++ src/advance/autoboxing/AutoBoxingDemo.java
@@ -0,0 +1,24 @@
+package advance.autoboxing;
+
+public class AutoBoxingDemo {
+    public static void main(String[] args) {
+        int i = 10;
+
+        //Autobox
+        Integer iObj = Integer.valueOf(i);
+        System.out.println("Value of Integer obj: " + iObj);
+
+        //auto-unbox
+        int i1 = iObj;
+        System.out.println("Value of i1: " + i1);
+
+        //Autobox
+        Character charObj = 'n';
+
+        //Auto-unbox
+        char ch = charObj;
+        System.out.println("Value of ch: " + ch);
+        System.out.println("Value of charObj: " + charObj);
+
+    }
+}
